0b6e6af4a4bbc0e30f675c5cbc4124a6c66a2eff
vigzmv <vigzmv@outlook.com>
2017-01-27 23:12:55 +0530

    CLoned :)

--- src/js/NewsItem.js
+++ src/js/NewsItem.js
@@ -1,7 +1,49 @@
 import React from 'react';
 import URL from 'url';
+import Moment from 'moment';
 
 export default class NewsItem extends React.Component {
+    getCommentLink() {
+        var commentText = 'discuss';
+        if (this.props.item.kids && this.props.item.kids.length) {
+            commentText = this.props.item.kids.length + ' comment';
+        }
+
+        return (
+            <a href={'https://news.ycombinator.com/item?id=' + this.props.item.id}>{commentText}</a>
+        );
+    }
+
+    getSubtext() {
+        return (
+            <div className="newsItem-subtext">
+                {this.props.item.score}
+                points by
+                <a href={'https://news.ycombinator.com/user?id=' + this.props.item.by}>{this.props.item.by}</a>
+                {Moment.utc(this.props.item.time * 1000).fromNow()}
+                | {this.getCommentLink()}
+            </div>
+        );
+    }
+
+    getRank() {
+        return (
+            <div className="newsItem-rank">
+                {this.props.rank}.
+            </div>
+        );
+    }
+
+    getVote() {
+        return (
+            <div className="newsItem-vote">
+                <a href={'https://news.ycombinator.com/vote?for=' + this.props.item.id + '&dir=up&goto=news'}>
+                    <img src="imgs/grayarrow.gif" width="10"/>
+                </a>
+            </div>
+        );
+    }
+
     getDomain() {
         return URL.parse(this.props.item.url).hostname;
     }
@@ -22,10 +64,14 @@ export default class NewsItem extends React.Component {
     render() {
         return (
             <div className="newsItem">
+                {this.getRank()}
+                {this.getVote()}
                 <div className="newsItem-itemText">
                     {this.getTitle()}
+                    {this.getSubtext()}
                 </div>
             </div>
         );
     }
+
 }

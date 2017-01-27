973f9989a18b8d5d9f2df72c4ec26e797e9a17c2
vigzmv <vigzmv@outlook.com>
2017-01-27 22:53:36 +0530

    Hacking News

--- src/js/NewsHeader.js
+++ src/js/NewsHeader.js
@@ -27,6 +27,52 @@ export default class NewsHeader extends React.Component {
             <div className="newsHeader">
                 {this.getLogo()}
                 {this.getTitle()}
+                {this.getNav()}
+                {this.getLogin()}
+            </div>
+        );
+    }
+
+    getNav() {
+        var navLinks = [
+            {
+                name: 'new',
+                url: 'newest'
+            }, {
+                name: 'comments',
+                url: 'newcomments'
+            }, {
+                name: 'show',
+                url: 'show'
+            }, {
+                name: 'ask',
+                url: 'ask'
+            }, {
+                name: 'jobs',
+                url: 'jobs'
+            }, {
+                name: 'submit',
+                url: 'submit'
+            }
+        ];
+
+        return (
+            <div className="newsHeader-nav">
+                {navLinks.map(function(navLink) {
+                    return (
+                        <a key={navLink.url} className="newsHeader-navLink newsHeader-textLink" href={"https://news.ycombinator.com/" + navLink.url}>
+                            {navLink.name}
+                        </a>
+                    );
+                })}
+            </div>
+        );
+    }
+
+    getLogin() {
+        return (
+            <div className="newsHeader-login">
+                <a className="newsHeader-textLink" href="https://news.ycombinator.com/login?goto=news">login</a>
             </div>
         );
     }
--- src/js/NewsItem.js
+++ src/js/NewsItem.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import URL from 'url';
 
 export default class NewsItem extends React.Component {
+    getDomain() {
+        return URL.parse(this.props.item.url).hostname;
+    }
+
+    getTitle() {
+        return (
+            <div className="newsItem-title">
+                <a className="newsItem-titleLink" href={this.props.item.url
+                    ? this.props.item.url
+                    : 'https://news.ycombinator.com/item?id=' + this.props.item.id}>{this.props.item.title}</a>
+                {this.props.item.url && <span className="newsItem-domain">
+                    <a href={'https://news.ycombinator.com/from?site=' + this.getDomain()}>({this.getDomain()})</a>
+                </span>}
+            </div>
+        );
+    }
+
     render() {
         return (
             <div className="newsItem">
-                I am NewsItem.
+                <div className="newsItem-itemText">
+                    {this.getTitle()}
+                </div>
             </div>
         );
     }

6050565ff4e43daabc780c704179837ac0adb950
vigzmv <vigzmv@outlook.com>
2017-01-28 21:49:11 +0530

    Got rid of JQuery and saved 200KB, . Pheww!! took a while Had to learn about promises and Fetch Api. after hours, got stuck with a array of promises and couldn't find a proper way to use the values inside them, then used an old school way to push the values inside and array and used the array when its lenght reached the required size. lol

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-import $ from 'jquery';
 import React from 'react'
 import {render} from 'react-dom';
 import NewsList from './NewsList.js';
@@ -6,21 +5,28 @@ import NewsHeader from './NewsHeader.js'
 
 render(
     <div>
-    <NewsHeader/>
-    <div className="loading">
-        <img className="gears" src="imgs/gears.gif" />
-    </div>
-</div>, document.querySelector('.content'));
+        <NewsHeader/>
+        <div className="loading">
+            <img className="gears" src="imgs/gears.gif"/>
+        </div>
+    </div>,
+ document.querySelector('.content')
+);
 
-function get(url) {
-    return Promise.resolve($.ajax(url));
-}
+const stories = [];
 
-get('https://hacker-news.firebaseio.com/v0/topstories.json').then(function(stories) {
-    return Promise.all(stories.slice(0, 30).map(itemId => get('https://hacker-news.firebaseio.com/v0/item/' + itemId + '.json')));
-}).then(function(items) {
-    render(
-        <NewsList items={items}/>, document.querySelector('.content'));
-}).catch(function(err) {
-    console.log('error occur', err);
+fetch('https://hacker-news.firebaseio.com/v0/topstories.json').then(parent => {
+    return parent.text();
+
+}).then(story => {
+    story.slice(1).split(",").slice(0, 30).map(itemsId => fetch('https://hacker-news.firebaseio.com/v0/item/' + itemsId + '.json').then(value => {
+        return value.json();
+    }).then(value => {
+        stories.push(value);
+        if (stories.length == 30) {
+            render(
+                <NewsList items={stories}/>, document.querySelector('.content'));
+        }
+        return items;
+    }));
 });
